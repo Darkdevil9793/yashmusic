c2696e437d06e088cc64c8a923505363d1da4f8b
darkdevil <120304886+Darkdevil9793@users.noreply.github.com>
2023-11-03 15:20:31 +0530

    Add files via upload

---
+++ modules/platforms/__init__.py
@@ -0,0 +1,13 @@
+# Power By @darkdevil898 & @ALEXBADHACKER
+# Join @darkdevil979 For More Update
+# Join @darkdevil898 For Hack
+# Join Our Chats @darkdevil979 & @darkdevil9793 
+
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
+I
