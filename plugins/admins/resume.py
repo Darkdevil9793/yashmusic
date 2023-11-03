8189b60b7fec98e74358761213d38e3bb97c7c2b
darkdevil <120304886+Darkdevil9793@users.noreply.github.com>
2023-11-03 15:31:13 +0530

    Add files via upload

---
+++ plugins/admins/resume.py
@@ -0,0 +1,41 @@
+# Power By @darkdevil898 & @ALEXBADHACKER
+# Join @darkdevil979 For More Update
+# Join @darkdevil898 For Hack
+# Join Our Chats @darkdevil979 & @darkdevil9793 
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from modules.config import BANNED_USERS
+from modules import app
+from modules.core.call import Aditya
+from modules.utils.helpers.filters import command
+from modules.utils.database import is_music_playing, music_on
+from modules.utils.decorators import AdminRightsCheck
+
+
+@app.on_message(
+    filters.command(["resume", "cresume"])
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def resume_com(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text("**❌ 𝐄𝐫𝐫𝐨𝐫, 𝐖𝐫𝐨𝐧𝐠 𝐔𝐬𝐚𝐠𝐞 𝐎𝐟 𝐂𝐨𝐦𝐦𝐚𝐧𝐝❗...**")
+    if await is_music_playing(chat_id):
+        return await message.reply_text("**🔊 𝐁𝐠𝐭 𝐌𝐮𝐬𝐢𝐜 𝐀𝐥𝐫𝐞𝐚𝐝𝐲 𝐏𝐥𝐚𝐲𝐢𝐧𝐠 ✨ ...**")
+    await music_on(chat_id)
+    await Aditya.resume_stream(chat_id)
+    await message.reply_text(
+        "**⏸ 𝐑𝐞𝐬𝐮𝐦𝐞𝐝 🌷 ...**\n\n⎿𝐑𝐞𝐪𝐮𝐞𝐬𝐭𝐞𝐝 𝐁𝐲 > {}".format(message.from_user.mention)
+    )
+
+
+
+# Power By @darkdevil898 & @ALEXBADHACKER
+# Join @darkdevil979 For More Update
+# Join @darkdevil898 For Hack
+# Join Our Chats @darkdevil979 & @darkdevil9793  
+@Adityadiscus 
