59ef42f092d720e295f6f793c6a6b7e931fe346c
darkdevil <120304886+Darkdevil9793@users.noreply.github.com>
2023-11-03 15:23:05 +0530

    Add files via upload

---
+++ modules/utils/__init__.py
@@ -0,0 +1,12 @@
+# Power By @darkdevil898 & @ALEXBADHACKER
+# Join @darkdevil979 For More Update
+# Join @darkdevil898 For Hack
+# Join Our Chats @darkdevil979 & @darkdevil9793 
+
+from .channelplay import *
+from .database import *
+from .decorators import *
+from .formatters import *
+from .inline import *
+from .pastebin import *
+from .sys import *
---
+++ modules/utils/exceptions.py
@@ -0,0 +1,9 @@
+# Power By @darkdevil898 & @ALEXBADHACKER
+# Join @darkdevil979 For More Update
+# Join @darkdevil898 For Hack
+# Join Our Chats @darkdevil979 & @darkdevil9793 
+
+
+class AssistantErr(Exception):
+    def __init__(self, errr: str):
+        super().__init__(errr)
