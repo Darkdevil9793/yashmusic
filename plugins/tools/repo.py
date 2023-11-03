19262891ebf3af45def4e34badb3674f2d2361a7
darkdevil <120304886+Darkdevil9793@users.noreply.github.com>
2023-11-03 16:45:22 +0530

    Add files via upload

---
+++ plugins/tools/repo.py
@@ -0,0 +1,46 @@
+# Power By @darkdevil898 & @ALEXBADHACKER
+# Join @darkdevil979 For More Update
+# Join @darkdevil898 For Hack
+# Join Our Chats @darkdevil979 & @darkdevil9793 
+
+from pyrogram import Client, filters
+
+from modules import app
+
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@app.on_message(
+    filters.command("repo")
+    & filters.group
+    & ~filters.edited & filters.group & ~filters.edited)
+async def repo(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/53acf0c78582f1f1ec71a.jpg",
+        caption=f"""🥀 𝐍𝐨𝐰 𝐌𝐲 𝐒𝐨𝐮𝐫𝐜𝐞 𝐂𝐨𝐝𝐞 𝐈𝐬 𝐏𝐮𝐛𝐥𝐢𝐜 🌺, 𝐂𝐥𝐢𝐜𝐤 𝐁𝐞𝐥𝐨𝐰 𝐑𝐞𝐩𝐨 𝐁𝐮𝐭𝐭𝐨𝐧 𝐅𝐨𝐫 𝐌𝐲 𝐂𝐨𝐝𝐞 ♕, 𝐈𝐟 𝐘𝐨𝐮 𝐖𝐚𝐧𝐭 𝐏𝐫𝐨𝐦𝐨𝐭𝐞 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩𝐬 𝐎𝐫 𝐎𝐭𝐡𝐞𝐫𝐬 𝐋𝐢𝐧𝐤, 𝐓𝐡𝐞𝐧 𝐂𝐥𝐢𝐜𝐤 𝐏𝐫𝐨𝐦𝐨𝐭𝐢𝐨𝐧 𝐁𝐮𝐭𝐭𝐨𝐧 𝐂𝐥𝐢𝐜𝐤 𝐎𝐭𝐡𝐞𝐫𝐬 𝐁𝐮𝐭𝐭𝐨𝐧 & 𝐉𝐨𝐢𝐧 𝐎𝐮𝐫 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐎𝐫 𝐆𝐫𝐨𝐮𝐩.. 🥀 𝐒𝐮𝐛𝐜𝐫𝐢𝐛𝐞 𝐎𝐮𝐫 𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥  :
+  [𝐘𝐨𝐮𝐭𝐮𝐛𝐞](https://youtube.com/@bikashgadgets)""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🥀 𝐑𝐞𝐩𝐨 🥀", url=f"https://github.com/Darkdevil9793/yashmusic")
+            ],          
+            [
+                    InlineKeyboardButton(
+                        "🥀 𝐏𝐫𝐨𝐦𝐨𝐭𝐢𝐨𝐧 🥀", url=f"https://t.me/darkdevil9793")
+                ],
+                [
+                    InlineKeyboardButton(
+                        "🥀 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 🥀", url=f"https://t.me/darkdevil979"
+                    ),
+                    InlineKeyboardButton(
+                        "🥀 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 🥀", url=f"https://t.me/darkdevil9793")
+                ]
+            ]
+        ),
+    ) 
+
+# Power By @darkdevil898 & @ALEXBADHACKER
+# Join @darkdevil979 For More Update
+# Join @darkdevil898 For Hack
+# Join Our Chats @darkdevil979 & @darkdevil9793 l9793 
