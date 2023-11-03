142d1271bc5823a21be76ab866ad2223fc5ec777
darkdevil <120304886+Darkdevil9793@users.noreply.github.com>
2023-11-03 15:18:22 +0530

    Add files via upload

---
+++ modules/config/__init__.py
@@ -0,0 +1,6 @@
+# Power By @darkdevil898 & @ALEXBADHACKER
+# Join @darkdevil979 For More Update
+# Join @darkdevil898 For Hack
+# Join Our Chats @darkdevil979 & @darkdevil9793 
+
+from .config import *
